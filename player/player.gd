1542e4b03be33148c784d3110b1039ee695a6dc2
VamsiKalyan SridharBabu <vamsisridhar@hotmail.com>
2022-07-16 02:36:48 +0100

    dash arrow

--- player/player.gd
+++ player/player.gd
@@ -4,7 +4,9 @@ export var speed: int = 200
 export var dash_speed: int = 2000
 export var dice_roll: int = 6
 var velocity: Vector2
+var dash_vector: Vector2
 var rng = RandomNumberGenerator.new()
+var arrow:bool = false
 
 func get_movement_input():
 	var velocity = Vector2()
@@ -27,13 +29,28 @@ func _on_dash_bar_ready_to_use(dc):
 var dash: bool = false;
 var dash_timer: float = 0;
 func _unhandled_input(event):
-	if(event.is_action_pressed("ui_left_click") && dash_ready):
+	if(event.is_action_pressed("ui_left_click")):
+		arrow = true
+	if(event.is_action_released("ui_left_click") && dash_ready):
 		dash = true;
 		dash_ready = false
 		dash_callback.reset_counter()
+		dash_vector = get_node(".").position - get_global_mouse_position()
+		arrow = false
 
 const epsilon = 0.0001
 func handle_animation():
+	
+	if(arrow):
+		$Arrow.visible = true
+		$Arrow.rotation = (get_node(".").position - get_global_mouse_position()).angle()+ PI/2
+		$Arrow.position = Vector2(
+			sin((get_node(".").position - get_global_mouse_position()).angle() + PI/2) * 100,
+			-cos((get_node(".").position - get_global_mouse_position()).angle() + PI/2) *100
+		)
+	else:
+		$Arrow.visible = false
+	
 	if(velocity.length_squared() < epsilon):
 		#idle
 		$AnimatedSprite.play("idle-"+ str(dice_roll))
@@ -66,7 +83,7 @@ func _physics_process(delta):
 	if dash:
 		dash = false
 		dash_timer = 0.2
-		var dash_dir = get_local_mouse_position().normalized()
+		var dash_dir = dash_vector.normalized()
 		velocity = dash_dir * dash_speed
 		rng.randomize()
 		dice_roll = rng.randi_range(1, 6)
