34b3701f05921cdde153c86b0a7edb1fc5250338
Vincent-Sietses <vincent.sietses@gmail.com>
2022-07-16 15:03:41 +0200

    ball collision jitter fix + loading

--- scripts/PoolBall.gd
+++ scripts/PoolBall.gd
@@ -8,49 +8,48 @@ func _ready():
 	add_to_group("balls")
 	mass = randi() % 4 + 12
 	self.set_gravity_scale(0.0)
-	#self.velocity.x  = 300 * randf()
-	#self.velocity.y  = 200 * randf()
-	self.apply_central_impulse( Vector2( 750 * randf()  , 750 * randf()  ) )
+	self.linear_damp = 0.05
+	
+	self.contact_monitor = true
+	self.contacts_reported = 1
+	
+	#self.apply_central_impulse( Vector2( 750 * randf()  , 750 * randf()  ) )
 	self.set_bounce(1.0)
+	 
 
 	
-#func _physics_process(delta):
-##	if get_slide_count() > 0:
-##		var collision = get_slide_collision(0)
-##		if collision != null:
-##			velocity = velocity.bounce(collision.normal)
-##
-##
-#	var coll_info = move_and_collide(self.velocity * delta)
-#
-#	if coll_info :
-#		var alpha = 0.95 # slowing down on ca
-#		var other = coll_info.collider
-#		# if hits the wall use normal collision
-#		var coll_direction = coll_info.normal 
-#
-#		if other.is_in_group("balls") : 
-#
-#			# collision normal using that both objects are circular
-#			coll_direction = (other.position - self.position).normalized()
-#			alpha = other.weight / self.weight
-#
-#			# velocity decomposition for self
-#			var self_along_collision = coll_direction.dot(self.velocity) * coll_direction
-#			var self_off_collision = self.velocity - self_along_collision
-#			print(alpha)
-#
-#			# velocity decomposition for other
-#			var other_along_collision = coll_direction.dot(other.velocity) * coll_direction
-#			var other_off_collision = other.velocity - other_along_collision 
-#
-#			#other.velocity = self_along_collision   + other_off_collision
-#			self.velocity = self_off_collision + other_off_collision 
-#
-#
-#			move_and_collide(self.velocity*delta)
-#			#other.move_and_collide(other.velocity)
-#
-#		else:
-#			self.velocity = alpha * self.velocity.bounce( coll_direction)
-#		#move_and_collide(self.velocity * delta)
+func _physics_process(delta):
+	if $Stopped_Timer.is_stopped():
+		if self.linear_velocity.length_squared() < 3000:
+			self.linear_damp = 0.1
+		if self.linear_velocity.length_squared() < 1000:
+			self.linear_damp = 0.3
+		if self.linear_velocity.length_squared() < 400 : 
+			self.linear_damp = 0.8
+			
+			$Stopped_Timer.wait_time = randf() * 3 + 0.5
+			$Stopped_Timer.start()
+			$Tween.interpolate_property(self,"modulate",self.modulate,Color.red,$Stopped_Timer.time_left,Tween.TRANS_LINEAR)
+			$Tween.start()
+	
+
+func _on_PoolBall_body_entered(body):
+	self.linear_velocity *= 0.95 # slow down a bit when colliding
+	
+	# code to stop "charging up" when hit 
+	#if not $Stopped_Timer.is_stopped():
+	#	self.linear_damp = 0.01
+	#	$Tween.stop(self)
+	#	self.modulate = Color(1,1,1)
+	#	$Stopped_Timer.stop()
+
+
+func _on_Stopped_Timer_timeout():
+	self.linear_damp = 0.05
+	
+	var angle = randf() * 2 * PI
+	var strength = 2 * randf() + 2
+	self.apply_central_impulse( 2e3* strength * Vector2( cos(angle)  , sin(angle)) )
+	$Tween.stop(self)
+	self.modulate = Color(1,1,1)
+	
